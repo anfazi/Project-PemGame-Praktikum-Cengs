a1a2f8ccf26ddd9db60306b069a1e0d992022944
Andika Fauzi Hadiana <doneka54@gmail.com>
2021-11-07 18:03:02 +0700

    Clock

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -5,21 +5,19 @@ using UnityEngine.UI; //energy dipakai format image
 
 public class HUDManager : MonoBehaviour
 {
+    //clock
+    public Text time;
+    //-----
+    //energy
     public Image currentEnergy;
-
     private GameObject player;
-
     private float energy = 200;
-
     private float maxEnergy = 200;
-
     private float kecepatan;
-
     private float kecepatanLari;
-
     private float input_x;
     private float input_z;
-
+    //-----
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +36,8 @@ public class HUDManager : MonoBehaviour
         input_z = player.GetComponent<Gerakan_Player>().z;
 
         EnergyDrain();
+        UpdateEnergy();
+        UpdateTime();
         
     }
 
@@ -60,5 +60,27 @@ public class HUDManager : MonoBehaviour
         float ratio = energy / maxEnergy;
 
         currentEnergy.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+
+    private void UpdateTime(){
+        int hours = EnviroSky.instance.GameTime.Hours;
+        int minutes = EnviroSky.instance.GameTime.Minutes;
+
+        string gameHours;
+        string gameMinutes;
+
+        if(hours >= 0 && hours < 10){
+            gameHours = "0" + hours.ToString();
+        }
+        else{
+            gameHours = hours.ToString();
+        }
+        if(minutes >= 0 && minutes < 10){
+            gameMinutes = "0" + minutes.ToString();
+        }
+        else{
+            gameMinutes = minutes.ToString();
+        }
+        time.text = gameHours + " : " + gameMinutes;
     }
 }
