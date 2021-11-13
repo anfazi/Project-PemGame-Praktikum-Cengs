1da0ea5cfe1a6cabf337e4b64608d87577b3bcf1
Andika Fauzi Hadiana <doneka54@gmail.com>
2021-11-13 07:02:01 +0700

    Main Menu, Lighting

---
+++ Assets/Scripts/AnimationFunction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationFunction : MonoBehaviour
+{
+   [SerializeField] MenuButtonController menuButtonController;
+    // Start is called before the first frame update
+   void PlaySound(AudioClip whichSound){
+       menuButtonController.audioSource.PlayOneShot(whichSound);
+   }
+}
---
+++ Assets/Scripts/Lighting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+    float time;
+    GameObject[] allLights;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allLights = GameObject.FindGameObjectsWithTag("MyLighting");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = EnviroSky.instance.GameTime.Hours;
+
+        if(time < 18 && time > 6){
+            foreach(GameObject i in allLights){
+                i.SetActive(false);
+            }
+        }
+            else{
+            foreach(GameObject i in allLights){
+                i.SetActive(true);    
+            }
+        }
+    }
+}
